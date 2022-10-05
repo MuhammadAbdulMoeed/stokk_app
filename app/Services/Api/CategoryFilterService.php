8fe606687113ec9b268d3588e14d408fcfe95642
khurram753 <mkhurramilyas@gmail.com>
2022-10-05 11:43:51 +0500

    merging two array with recursive distinct

--- app/Services/Api/CategoryFilterService.php
+++ app/Services/Api/CategoryFilterService.php
@@ -72,7 +72,7 @@ class CategoryFilterService
                             ->whereIn('category_id',$getCategory->subCategory->pluck('id')->toArray())
                             ->get()->toArray();
 
-                        $getSubCategories = array_diff($getCategoriesBrands,$getSubCategoriesBrands);
+                        $getSubCategories = array_merge_recursive_distinct($getCategoriesBrands,$getSubCategoriesBrands);
 
 
 
