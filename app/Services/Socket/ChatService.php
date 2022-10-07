5e2af7fdb37c1cd5e3e185740c71c87a128c7dbe
khurram753 <mkhurramilyas@gmail.com>
2022-10-07 11:14:39 +0500

    change event name in socket

--- app/Services/Socket/ChatService.php
+++ app/Services/Socket/ChatService.php
@@ -37,7 +37,7 @@ class ChatService
         if (sizeof($conversations) > 0) {
 
 
-            $socket->emit('conversationList', [
+            $io->emit($data['user_id'].'-conversationList', [
                 'result' => 'success',
                 'message' => 'Conversation Found',
                 'data' => $conversations
@@ -51,7 +51,7 @@ class ChatService
 
         } else {
 
-            $socket->emit('conversationList', [
+            $io->emit($data['user_id'].'-conversationList', [
                 'result' => 'success',
                 'message' => 'No Conversation Found',
                 'data' => []
@@ -256,7 +256,7 @@ class ChatService
                 $getReceiverConversation = $this->chatService->findUserChat($data['receiver_id']);
 
                 if (sizeof($getReceiverConversation) > 0) {
-                    return $socket->emit('conversationList', [
+                    return $io->emit($getReceiver->socket_id.'-conversationList', [
                             'result' => 'success',
                             'message' => 'Conversation Found',
                             'data' => $getReceiverConversation
